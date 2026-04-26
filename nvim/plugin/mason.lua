e7131b916b67be1e4d0ed8837c3369e47ecd94d6
Joni Rämö <yotara91@gmail.com>
2026-04-27 00:01:06 +0300

    Update Mason config with default linters

--- nvim/plugin/mason.lua
+++ nvim/plugin/mason.lua
@@ -24,21 +24,26 @@ local servers = {
 	},
 }
 
+-- This ensures which linters will be installed by default
 local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
-	"stylua", -- Used to format Lua code
+	"stylua",
+	"markdownlint",
+	"jsonlint",
+	"stylelint",
+	"eslint",
+	"yamllint",
 })
 require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 require("mason-lspconfig").setup({
-	ensure_installed = {}, -- explicitly set to empty, installs are managed via mason-tool-installer
+	ensure_installed = {}, -- Explicitly set to empty, installs are managed via mason-tool-installer
 	automatic_installation = false,
 	handlers = {
 		function(server_name)
 			local server = servers[server_name] or {}
-			-- This handles overriding only values explicitly passed
-			-- by the server configuration above. Useful when disabling
-			-- certain features of an LSP (for example, turning off formatting for ts_ls)
+			-- This handles overriding only values explicitly passed by the server configuration above.
+			-- Useful when disabling certain features of an LSP (for example, turning off formatting for ts_ls)
 			server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 			require("lspconfig")[server_name].setup(server)
 		end,
